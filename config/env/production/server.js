a0359694f574012cf5dd8e6d491d7ebb28a76457
Arno De Pachter <arno@zeelandmultimedia.nl>
2021-10-05 17:43:34 +0200

    ok

---
+++ config/env/production/server.js
@@ -0,0 +1,3 @@
+module.exports = ({ env }) => ({
+    url: env('https://inkosmart.herokuapp.com'),
+  });
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'aed35452-66e1-4837-bdb4-265722882133'
+};
