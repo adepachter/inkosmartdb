275f5c4959691a4c753dbc54fc29116bbef01172
Arno De Pachter <arno@zeelandmultimedia.nl>
2021-10-10 14:16:36 +0200

    deleted server.js

--- config/env/production/server.js
+++
@@ -1,3 +0,0 @@
-module.exports = ({ env }) => ({
-    url: env('https://inkosmart.herokuapp.com'),
-  });
--- config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'da848d346ff559f150b714134df52d73'),
-    },
-  },
-});
